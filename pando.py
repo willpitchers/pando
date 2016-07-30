9b6077add514b25d7239209517a9d3ab10377628
Mark Schultz <mark.schultz@unimelb.edu.au>
2016-07-30 18:15:14 +1000

    Update pando.py

--- pando.py
+++ pando.py
@@ -35,10 +35,11 @@ from multiprocessing import Pool
 
 # set up the arguments parser to deal with the command line input
 PARSER = argparse.ArgumentParser(description="CPE ongoing")
-PARSER.add_argument("-i", "--mdu_read_ids", help="One MDU-ID per line.",
+PARSER.add_argument("-i", "--mdu_read_ids", help="One MDU-ID per line.\
+                    Use full path or put in same folder as run folder.",
                     required=True)
 PARSER.add_argument("-w", "--wgs_qc", help="Path to WGS\
-                    QC. Default '/mnt/seq/MDU/QC/'", 
+                    QC. Default '/mnt/seq/MDU/QC/'",
                     default="/mnt/seq/MDU/QC/", required=False)
 PARSER.add_argument("-d", "--delete_tempdirs", help="Delete tempdirs created\
                     during run? Default = 'no'.", default='no', required=False)
@@ -47,7 +48,7 @@ PARSER.add_argument("-t", "--threads", help='Number of threads, default=\'72\'',
 PARSER.add_argument("-a", "--andi_run", help='Run andi phylogenomic analysis?\
                     Default=\'yes\'', default='yes', required=False)
 PARSER.add_argument("-m", "--model_andi_distance", help='Substitution model.\
-                    \'Raw\', \'JC\', or \'Kimura\'. Default = \'JC\'.', 
+                    \'Raw\', \'JC\', or \'Kimura\'. Default = \'JC\'.',
                     default='JC', required=False)
 PARSER.add_argument('-c', '--percent_cutoff', help='For abricate, call the\
                     gene \'present\' if greater than this value and \'maybe\'\
@@ -55,17 +56,15 @@ PARSER.add_argument('-c', '--percent_cutoff', help='For abricate, call the\
                     \'100\', not \'1\'.', default=95, type=int, required=False)
 PARSER.add_argument('-e', '--email_addresses', help='Email addresses to send\
                     results (comma or space delimited)', nargs = '+',
-                    default = None, required = False)
+                    required = True)
 PARSER.add_argument('-j', '--job_number', help='Enter the MDU job number\
-                    (no spaces).', required = True)
-PARSER.add_argument('-n', '--investigator_name', help='Name of investigator\
                     (no spaces).', required = True)
 
 ARGS = PARSER.parse_args()
 
 
 #Add MLST schemes to force their usage if that species is encountered
-mlst_schemes = {"Acinetobacter baumannii": "abaumannii",
+MLST_SCHEMES = {"Acinetobacter baumannii": "abaumannii",
 "Citrobacter freundii": "cfreundii",
 "Cronobacter": "cronobacter",
 "Enterobacter cloacae": "ecloacae",
@@ -73,11 +72,6 @@ mlst_schemes = {"Acinetobacter baumannii": "abaumannii",
 "Klebsiella oxytoca": "koxytoca",
 "Klebsiella pneumoniae": "kpneumoniae",
 "Pseudomonas aeruginosa": "paeruginosa"}
-
-#print mlst_schemes
-
-
-abricate_headers = []
 
 
 class Isolate(object):
@@ -138,13 +132,13 @@ class Isolate(object):
         abricate_results = pd.DataFrame([ab_results], index=[self.id])
 #         print abricate_results
         return abricate_results
-        
+       
     def mlst(self, species, assembly):
         '''
         Store the MLST.
         '''
-        if species in mlst_schemes:
-            cmd = 'mlst --scheme '+mlst_schemes[species]+' --quiet '+assembly
+        if species in MLST_SCHEMES:
+            cmd = 'mlst --scheme '+MLST_SCHEMES[species]+' --quiet '+assembly
         else:
             cmd = 'mlst --quiet '+assembly
         args_mlst = shlex.split(cmd)
@@ -152,7 +146,7 @@ class Isolate(object):
         proc = Popen(args_mlst, stdout = PIPE)
         output = proc.stdout.read()
         #should write a function here too
-        if species in mlst_schemes:
+        if species in MLST_SCHEMES:
             mlst = output.rstrip().split('\n')
             mlst = [line.strip().split('\t') for line in filter(None, mlst)]
             header = mlst[0]
@@ -189,12 +183,12 @@ class Isolate(object):
         args_grep = shlex.split(cmd_grep)
         args_sort = shlex.split(cmd_sort)
         args_head = shlex.split(cmd_head)
-        
+       
         #Pipe the output of one args to another
         proc1 = Popen(args_grep, stdout = PIPE)
         proc2 = Popen(args_sort, stdin=proc1.stdout, stdout=PIPE)
         proc3 = Popen(args_head, stdin=proc2.stdout, stdout=PIPE)
-        
+       
         output = proc3.stdout.read()
         kraken = output.rstrip().split('\n')
         kraken = [line.strip().split('\t') for line in filter(None, kraken)]
@@ -205,7 +199,6 @@ class Isolate(object):
         Get the kraken best hit from assemblies.
         '''
         #Pipe these commands together
-#         kraken --fasta-input /mnt/seq/MDU/QC/2016-15685/contigs.fa | kraken-report | grep -P '\tS\t' | sort -k 1 -r | head -3
         cmd_kraken = 'nice kraken --threads 4 --db /bio/db/kraken/minikraken --fasta-input /mnt/seq/MDU/QC/'+self.id+'/contigs.fa'
         cmd_krk_r = 'kraken-report'
         cmd_grep = "grep -P '\tS\t'"
@@ -218,14 +211,14 @@ class Isolate(object):
         args_grep = shlex.split(cmd_grep)
         args_sort = shlex.split(cmd_sort)
         args_head = shlex.split(cmd_head)
-        
+       
         #Pipe the output of one args to another
         proc1 = Popen(args_kraken, stdout = PIPE)
         proc2 = Popen(args_krk_report, stdin=proc1.stdout, stdout=PIPE, stderr=PIPE)
         proc3 = Popen(args_grep, stdin=proc2.stdout, stdout=PIPE, stderr=PIPE)
         proc4 = Popen(args_sort, stdin=proc3.stdout, stdout=PIPE, stderr=PIPE)
         proc5 = Popen(args_head, stdin=proc4.stdout, stdout=PIPE, stderr=PIPE)
-        
+       
         output = proc5.stdout.read()
         kraken = output.rstrip().split('\n')
         kraken = [line.strip().split('\t') for line in filter(None, kraken)]
@@ -249,7 +242,7 @@ class Isolate(object):
             else:
                 species = 'indet'
             return species
-        
+
     def species_conclusion(self, kraken_reads_species, kraken_contigs_species):
         '''
         Make a decision on the species call as consensus of reads- and contigs-
@@ -303,7 +296,7 @@ def read_file_lines(file_name):
     '''
     file_contents = [line.rstrip().split() for line in open(file_name).readlines()]
     return file_contents
-    
+
 def lower_tri(full_matrix):
     '''
     Take a symmetrical matrix, convert it to a lower triangle _Matrix object.
@@ -351,18 +344,18 @@ def kraken_contigs_multiprocessing(iso):
 
 def main():
     '''
-    Read in the MDU-IDs from file. For each id, instantiate an object of 
-    class Isolate.  This class associates all of the QC data with the ID tag.  
-    Move the contigs for all isolates into a tempdir, with a 9-character 
+    Read in the MDU-IDs from file. For each id, instantiate an object of
+    class Isolate.  This class associates all of the QC data with the ID tag. 
+    Move the contigs for all isolates into a tempdir, with a 9-character
     filename.  Run andi phylogenomics on all the contig sets.  Infer an NJ tree
-    using Bio Phylo from the andi-calculated distance matrix.  Correct the 
+    using Bio Phylo from the andi-calculated distance matrix.  Correct the
     negative branch lengths in the NJ tree using ETE2.  Export the tree to file.
     Export the metadata to file.  Todo: plot the metadata next to tree.'''
-    
+
     ids = isolate_request_ids(ARGS.mdu_read_ids)
 #     print ids
     isos = isolates_available(ids)
-    
+
     #make tempdir and store the contigs there for 'andi' analysis
     assembly_tempdir = make_tempdir()
     print '\nMoving contigs into temp place:'
@@ -389,29 +382,29 @@ def main():
                   ARGS.threads+' '+assembly_tempdir+'/*_contigs.fa > '+andi_mat
         print '\nRunning andi with: \''+andi_c+'\''
         os.system(andi_c)
-        
+
         #Read in the andi dist matrix, convert to lower triangle
         dm = read_file_lines(andi_mat)[1:]
         dm = lower_tri(dm)
-        
+       
         #Dict to store kraken_contig results
         kraken_contigs_dict = {key: [] for (key) in isos}
-        
+       
         #Kraken set at 4 threads, so 18 processes can run on 72 CPUs
         n_isos = len(isos)
         print '\nRunning kraken on the assemblies:'
         if n_isos <= 18:
             p = Pool(n_isos)
-            results = p.map(kraken_contigs_multiprocessing, isos)
-            for result in results:
+            results_k = p.map(kraken_contigs_multiprocessing, isos)
+            for result in results_k:
                 kraken_contigs_dict[result[0]].extend([result[1], result[2]])
         if n_isos > 18:
-            p = Pool(18) 
-            results = p.map(kraken_contigs_multiprocessing, isos)
-            for result in results:
+            p = Pool(18)
+            results_k = p.map(kraken_contigs_multiprocessing, isos)
+            for result in results_k:
                 kraken_contigs_dict[result[0]].extend([result[1], result[2]])
 #         print kraken_contigs_dict
-
+        #Need to parallelise the MLST step too. 
         summary_frames = []
         #These steps replaces the dm-matrix short names with original names
         for iso in isos:
@@ -443,21 +436,21 @@ def main():
             iso_df.append(kraken_summary_df)
             metadata_iso = pd.concat(iso_df, axis=1)
 #             print metadata_iso
-            summary_frames.append(metadata_iso) 
-            
+            summary_frames.append(metadata_iso)
+
             for i in range(0, len(dm.names)):
                 #iso_id_trans[iso] is the short_id
                 if dm.names[i] == iso_id_trans[iso]:
-                    #change next line to just 'iso' after debug
-                    dm.names[i] = iso #+'__'+species_reads.replace(' ', '_')+'__'+str(reads_pc)
+                    dm.names[i] = iso
+        print kraken_summary_dict
         metadata_all = pd.concat(summary_frames)
         metadata_all.fillna('.', inplace=True)
         print metadata_all
         #Write metadata to csv and tsv
         csv = base+'_metadataAll.csv'
         tsv = base+'_metadataAll.tab'
-        metadata_all.to_csv(csv, mode='w', index=True, index_label = 'name')
-        metadata_all.to_csv(tsv, mode='w', sep='\t', index=True, index_label = 'name')
+        metadata_all.to_csv(csv, mode='w', index=True, index_label='name')
+        metadata_all.to_csv(tsv, mode='w', sep='\t', index=True, index_label='name')
         #Write out the tip name tempfile translations in case of bug
         with open(base+'_tipNamesTranslated.csv', 'w') as tip_names:
             for key, value in iso_id_trans.items():
@@ -482,15 +475,16 @@ def main():
         os.remove('temp.tre')
         #Add an option to import metadata to merge with the output
         #Email the results
-        if ARGS.email_addresses != None:
-            phandango='https://jameshadfield.github.io/phandango/'
-            cmd_mail = 'mail -s \''+ARGS.job_number+'\' -a '+t_out+' -a '+\
-                csv+' -a '+tsv+' '+','.join(ARGS.email_addresses)+' <<< \'Hi,\
-                \n\nPlease find attached the results for job '+\
-                ARGS.job_number+'. To view the results, open '+phandango+' and then simply drag and drop the attached .tre and .csv files into that window.  Alternatively, load the .tre in FigTree and import the annotations in the .tab file.\n\nGood luck with your investigations,\n\nPando.\n\nInvestigator name: '+ARGS.investigator_name+'\''
-            print '\nSending you your results via email using the command:'
+        #todo: wrap line widths
+        phandango='https://jameshadfield.github.io/phandango/'
+        cmd_mail = 'mail -s \''+ARGS.job_number+'\' -a '+t_out+' -a '+\
+            csv+' -a '+tsv+' '+','.join(ARGS.email_addresses)+' <<< \'Hi,\
+            \n\nPlease find attached the results for job '+\
+            ARGS.job_number+'. To view the results, open '+phandango+' and then simply drag and drop the attached .tre and .csv files into that window.  Alternatively, load the .tre in FigTree and import the annotations in the .tab file.\n\nGood luck with your investigations,\n\nPando.\''
 #             print cmd_mail
-            os.system(cmd_mail)
+        os.system(cmd_mail)
+        print '\nResults sent via email.'
+
     #Delete the tempdirs created during the run
     if 'y' in ARGS.delete_tempdirs.lower():
         shutil.rmtree(assembly_tempdir, ignore_errors=True)
