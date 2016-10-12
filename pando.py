01f1b0bd688fe8650393e4c54be9d38320cda389
Mark Schultz <mark.schultz@unimelb.edu.au>
2016-10-12 22:54:06 +1100

    Update pando.py

--- pando.py
+++ pando.py
@@ -11,7 +11,7 @@ Specific for MDU folder structures and QC
 Optionally run roary analysis.
 Email: dr.mark.schultz@gmail.com
 Github: https://github.com/schultzm
-YYYMMDD_HHMM: 20160915_1300
+YYYMMDD_HHMM: 20161012_2300
 
 Acknowledgements:
 Torsten Seemann (pando relies heavily on Torsten's tools)
@@ -59,7 +59,7 @@ import pandas as pd
 from ete3 import Tree
 
 
-VERSION = 'pando version 2.3.2'
+VERSION = 'pando version 2.3.3'
 
 
 # set up the arguments parser to deal with the command line input
